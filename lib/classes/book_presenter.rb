5f88411d8d42fc531b39e0952514e9842b564058
Kyle Tolle <kyle@nullsix.com>
2015-05-03 14:10:27 -0600

    Add show action to book presenter

--- lib/classes/book_presenter.rb
+++ lib/classes/book_presenter.rb
@@ -9,41 +9,73 @@ class BookPresenter
   attr_accessor :current_user
 
   def index_haml(current_user)
-    self.current_user = current_user
+    @current_user = current_user
 
-    book_index_haml = <<EOB
+    book_index_haml = <<HAML
 %tr
   %td= book.title
   %td= book.author.name
-#{read_link}
-#{action_links}
-EOB
+#{index_read_link}
+#{index_action_links}
+HAML
     Haml::Engine.new(book_index_haml).render(self)
   end
 
+  def show_haml(current_user)
+    @current_user = current_user
+
+    book_show_haml = <<HAML
+#{action_links.join(' ')}
+HAML
+    Haml::Engine.new(book_show_haml).render(self)
+  end
+
 private
-  def read_link
+  def index_read_link
 <<HAML
-  %td= view.link_to 'Read', book
+  %td #{read_link}
 HAML
   end
 
-  def action_links
+  def read_link
+    view.link_to 'Read', book
+  end
+
+  def index_action_links
+    action_links do |link|
+<<HAML
+  %td #{link}
+HAML
+    end.join('')
+  end
+
+  def action_links(&block)
     user_is_author      = current_user == book.author
     reader_can_purchase = !!current_user
+    links = []
     if user_is_author
-<<HAML
-  %td= view.link_to 'Edit', view.edit_book_path(book)
-  %td= view.link_to('Delete', book, method: :delete, data: { confirm: 'Are you sure?' })
-HAML
+      links << edit_link
+      links << delete_link
     elsif reader_can_purchase
-<<HAML
-  %td= view.link_to 'Purchase', '#'
-HAML
-    else
-<<HAML
-  %td &nbsp;
-HAML
+      links << purchase_link
     end
+
+    if block_given?
+      links.map!{|link| yield link}
+    end
+
+    links
+  end
+
+  def edit_link
+    view.link_to 'Edit', view.edit_book_path(book)
+  end
+
+  def delete_link
+    view.link_to 'Delete', book, method: :delete, data: { confirm: 'Are you sure?' }
+  end
+
+  def purchase_link
+    view.link_to 'Purchase', '#'
   end
 end
